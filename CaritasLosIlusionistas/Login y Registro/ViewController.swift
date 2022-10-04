c6de1288e13ba54700e3ffa3838a8e6cb5a06ed2
jaiovi <sebastianjo2011@gmail.com>
2022-10-04 15:32:44 -0500

    alertas en login ya listas

--- CaritasLosIlusionistas/Login y Registro/ViewController.swift
+++ CaritasLosIlusionistas/Login y Registro/ViewController.swift
@@ -43,6 +43,7 @@ class ViewController: UIViewController {
         var nombreCompleto = ""
         
         var loginExitoso = false
+        var notificacion = ""
         
         // CHECA SI HAY ESPACIOS VACIOS
         if (lbEmail != nil && lbPass.text != ""){
@@ -50,79 +51,86 @@ class ViewController: UIViewController {
             contra = lbPass.text!
         }else{
             //emailBuscar.text = "hugo"
-            print("Uno de los campos esta vacío")
+            alertas(titulo: "Aviso", texto: "Uno de los campos esta vacio")
             return loginExitoso
         }
         //INICIA API
         guard let url = URL(string:"https://equipo05.tc2007b.tec.mx:10210/usuarios/login?emailUsuarios=\(emailBuscar)")
-        else { return loginExitoso }
+        else {
+            return loginExitoso
+        }
         
-         /*
-           https://pokeapi.co/api/v2/pokemon/ditto
-           https://shakira.tc2007b.tec.mx:10201/user?username=hugo
-         */
-            //var respuestaUsuario = ""
-        
-            let grupo = DispatchGroup()
-            grupo.enter()
-        
-            let task = URLSession.shared.dataTask(with: url){
-                data, response, error in
-                
-                /*
-                if let data = data, let string = String(data: data, encoding: .utf8){
-                    print(string)
-                }
-                 */
-                
-                let decoder = JSONDecoder()
+        let grupo = DispatchGroup()
+        grupo.enter()
+    
+        let task = URLSession.shared.dataTask(with: url){
+            data, response, error in
+            
+            /*
+            if let data = data, let string = String(data: data, encoding: .utf8){
+                print(string)
+            }
+             */
+            
+        let decoder = JSONDecoder()
 
-                        if let data = data{
-                            do{
-                                let tasks = try decoder.decode([Usuario].self, from: data)
-                                if (!tasks.isEmpty){
-                                    tasks.forEach{ i in
-                                        print("-------- USUARIO ---------")
-                                        print("Id Usuario: \(i.idUsuarios)" )
-                                        print("Password: \(i.passUsuarios)" )
-                                        print("Nombre: \(i.nombreUsuarios)" )
+                    if let data = data{
+                        do{
+                            let tasks = try decoder.decode([Usuario].self, from: data)
+                            if (!tasks.isEmpty){
+                                tasks.forEach{ i in
+                                    print("-------- USUARIO ---------")
+                                    print("Id Usuario: \(i.idUsuarios)" )
+                                    print("Password: \(i.passUsuarios)" )
+                                    print("Nombre: \(i.nombreUsuarios)" )
+                                    
+                                    idUsuario = Int(i.idUsuarios)
+                                    nombreCompleto = "\(i.nombreUsuarios) \(i.apellidoPaterno) \(i.apellidoPaterno)"
+                                    
+                                    if(i.passUsuarios == contra){
+                                        print("Login exitoso")
+                                        //METAN TODOS LOS DATOS A DEFAULTS y activarle el segue
+                                        self.defineDefaults_Login(id: idUsuario, email: emailBuscar, nombreCompleto: nombreCompleto)
+                                        loginExitoso = true
+                                    }else{
+                                        notificacion = "Contraseña incorrecta"
                                         
-                                        idUsuario = Int(i.idUsuarios)
-                                        nombreCompleto = "\(i.nombreUsuarios) \(i.apellidoPaterno) \(i.apellidoPaterno)"
-                                        
-                                        if(i.passUsuarios == contra){
-                                            print("Login exitoso")
-                                            //METAN TODOS LOS DATOS A DEFAULTS y activarle el segue
-                                            self.defineDefaults_Login(id: idUsuario, email: emailBuscar, nombreCompleto: nombreCompleto)
-                                            loginExitoso = true
-                                        }else{
-                                            print("Contraseña incorecta")
-                                            
-                                        }
-                                        //respuestaUsuario = "USUARIO: Id = \(i.idUsers), Username: \(i.username), Password: \(i.password)"
                                     }
-                                }else{
-                                    //respuestaUsuario = "Usuario NO Encontrado"
-                                    print("----- USUARIO NO ENCONTRADO -----")
+    
                                 }
-                            }catch{
-                                print(error)
+                            }else{
+                                //respuestaUsuario = "Usuario NO Encontrado"
+                                notificacion = "Usuario no encontrado"
                             }
+                        }catch{
+                            print(error)
                         }
+                    }
                 grupo.leave()
             }
 
             task.resume()
         
             grupo.wait()
+            if notificacion != ""{
+                alertas(titulo: "Aviso", texto: notificacion)
+            }
+            
             //lbRespuesta.text = respuestaUsuario
             return loginExitoso
     }
     
-    func defineDefaults_Login( id : Int , email : String , nombreCompleto : String ){
+    func defineDefaults_Login( id : Int , email : String , nombreCompleto : String )->Void{
         defaults.setValue(id, forKey: "idUsuarios")
         defaults.setValue(email, forKey: "emailUsuarios")
         defaults.setValue(nombreCompleto, forKey: "nombreCompleto")
+    }
+    
+    func alertas(titulo:String , texto:String) ->Void{
+        let alerta = UIAlertController(title:titulo, message: texto, preferredStyle: .alert)
+        let botonCancel = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alerta.addAction(botonCancel)
+        present(alerta, animated: true)
     }
     
 }
